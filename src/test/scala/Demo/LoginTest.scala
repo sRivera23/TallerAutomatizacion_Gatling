4bf0491e8aff40b644f3e0189b9df426208e583f
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-06-24 21:09:27 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -16,7 +16,7 @@ class LoginTest extends Simulation{
   val scn = scenario("Login").
     exec(http("login")
       .post(s"users/login")
-      .body(StringBody("""{"email": "${email}", "password": "${password}"}""")).asJson
+      .body(StringBody("""{"email": $email, "password": $password}""")).asJson
       //Validar status 200 del servicio
       .check(status.is(200))
       .check(jsonPath("$.token").saveAs("authToken"))

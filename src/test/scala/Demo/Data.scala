8449cba95b61720af694b22d6cce24ea5bc56f08
chrisystems <51771601+chrisystems@users.noreply.github.com>
2025-06-26 18:16:03 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,6 +2,6 @@ package Demo
 
 object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com/"
-    val email = "test@testudeachris.com"
+    val email = "pruebasudea@test.com"
     val password = "12345678"
 }
